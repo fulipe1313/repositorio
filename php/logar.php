a2e2f9c0da7a3c6b5fa98f4b4bf7e1339474e004
fulipe1313 <pedruop18@gmail.com>
2023-04-27 11:40:34 -0300

    ye

--- php/logar.php
+++ php/logar.php
@@ -5,15 +5,15 @@
  $nome = $_POST['nome']; 
  $senha = $_POST['senha']; 
  
- 
- $query = "SELECT * FROM usuarios where usuario = '{$nome}' and senha = '{$senha}'";
+
+ $query = "SELECT * FROM usuarios where usuario = '$nome' and senha = '$senha'";
  $resultado = mysqli_query($conexao, $query);
  $user = mysqli_fetch_assoc($resultado); 
 
  if(mysqli_num_rows ($resultado) > 0){
      
      $_SESSION['nome'] = $user['nome'];
-     $_SESSION['id'] = $user['id'];
+    # $_SESSION['id'] = $user['id'];
     
       header("location: ../index.php");
      
